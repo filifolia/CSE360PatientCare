ce350b59512991f28ed8f835d114d1fec11589f4
Michael Hadder <Tiberius168@users.noreply.github.com>
2015-04-16 23:44:04 -0700

    Create Reference.java
    Created the reference object.

---
+++ src/Reference.java
@@ -0,0 +1,67 @@
+
+public class Reference 
+{
+	private int key;
+	private String username;
+	private boolean list;
+	public Reference next;
+	//False of Patient, true if CareGiver
+	
+	public Reference()
+	{
+		key = 0;
+		username = null;
+		list = false;
+		next = null;
+	}
+	
+	public Reference(int code, String name, boolean type)
+	{
+		key = code;
+		username = name;
+		list = type;
+		next = null;
+	}
+	
+	//ALL setters
+	public void setKey(int code)
+	{
+		key = code;
+	}
+	
+	public void setUsername(String name)
+	{
+		username = name;
+	}
+	
+	public void setList(boolean type)
+	{
+		list = type;
+	}
+	
+	public void setNext(Reference next)
+	{
+		this.next = next;
+	}
+	
+	//ALL getters
+	public int getKey()
+	{
+		return key;
+	}
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public boolean checkList()
+	{
+		return list;
+	}
+	
+	public Reference getNext()
+	{
+		return next;
+	}
+}
