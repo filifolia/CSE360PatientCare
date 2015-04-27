749816f46f76dfd23ae575e60dda783a9476e77f
filifolia <willynetros@ymail.com>
2015-04-27 14:07:39 -0700

    Removed training variable as it is useless

--- src/CareGiver.java
+++ src/CareGiver.java
@@ -1,23 +1,17 @@
 public class CareGiver extends User 
 {
-	private String training;
 	private int numOfPatients;
 	private String degree;
 	//private Reference[] patientList;
 	
 	public CareGiver()
 	{
-		training = null;
 		numOfPatients = 0;
 		degree = null;
 		//patientList = new Reference[] patientList
 	}
 	
 	//ALL setters and junk
-	public void setTraining(String words)
-	{
-		training = words;
-	}
 	public void setNumOfPatients(int num)
 	{
 		numOfPatients = num;
@@ -28,10 +22,6 @@ public class CareGiver extends User
 	}
 	
 	//ALL getters
-	public String getTraining()
-	{
-		return training;
-	}
 	public int getNumOfPatients()
 	{
 		return numOfPatients;
